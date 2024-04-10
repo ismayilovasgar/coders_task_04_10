a9ca0a73c8bfc769e7b311542da39ce04d292e5e
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-04-10 18:28:30 +0400

    part_1

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ const add_item_btn = document.getElementById("add_item");
 const tbody = document.createElement("tbody");
 const tr_tbody = document.createElement("tr");
 tr_tbody.classList.add("tr_tbody");
+//.... elave edecem
