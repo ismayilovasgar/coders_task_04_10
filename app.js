899cfd7fcf97afcfefcd292ef31247c7be363ccc
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-04-10 18:27:37 +0400

    install

---
+++ app.js
@@ -0,0 +1,7 @@
+const thead = document.getElementById("thead");
+const add_item_btn = document.getElementById("add_item");
+
+//* add operation items
+const tbody = document.createElement("tbody");
+const tr_tbody = document.createElement("tr");
+tr_tbody.classList.add("tr_tbody");
